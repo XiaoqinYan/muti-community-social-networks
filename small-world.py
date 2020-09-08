e138bdfc772eeaa29c4f2e1db8b97ade8ff3262c
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-09-08 14:03:02 -0400

    Update small-world.py

--- small-world.py
+++ small-world.py
@@ -42,3 +42,13 @@ def set_node_community(G, communities):
         else:
             # External edge, mark as 0
             G.edges[v, w]['community'] = 0
+
+            def get_color(i, r_off=1, g_off=1, b_off=1):
+    r0, g0, b0 = 0, 0, 0
+    n = 16
+    low, high = 0.1, 0.9
+    span = high - low
+    r = low + span * (((i + r_off) * 3) % n) / (n - 1)
+    g = low + span * (((i + g_off) * 5) % n) / (n - 1)
+    b = low + span * (((i + b_off) * 7) % n) / (n - 1)
+    return (r, g, b)
