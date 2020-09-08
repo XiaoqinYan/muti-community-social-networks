cdb3b06362f8653843e6d5fb35a72dbea1d0ea56
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-09-08 14:00:45 -0400

    Update small-world.py

--- small-world.py
+++ small-world.py
@@ -17,3 +17,9 @@ G = nx.disjoint_union(G_1, G_2) #merge (G_1 to G_N) as communities of G.
 
 
 l=rnd.randint(1,n)
+
+or i in range(l):#add random edges to make the entire society connected
+    
+    u=rnd.randint(0, n-1)
+    v=rnd.randint(n, 2*n-1)
+    G.add_edge(u,v)
