646cfff7a605f4d12248130ad91df336aef188db
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-09-08 14:00:00 -0400

    Create small-world.py

---
+++ small-world.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import numpy as np
+import random as rnd
+import matplotlib.pyplot as plt
+from networkx.algorithms.community import greedy_modularity_communities
+
+rnd.seed(19680801)
+np.random.seed(19680801)
+
+n=300
+
+G_1 = nx.watts_strogatz_graph(n, 3, 0.05)
+G_2 = nx.watts_strogatz_graph(n, 3, 0.05)
+
+
+G = nx.disjoint_union(G_1, G_2) #merge (G_1 to G_N) as communities of G. 
+
+
+l=rnd.randint(1,n)
