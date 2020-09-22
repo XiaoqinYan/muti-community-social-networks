5eceb7ad90863e84fd9f98d17eff181f4945fdf7
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-09-22 15:15:48 -0400

    Update join-comunity.py

--- join-comunity.py
+++ join-comunity.py
@@ -9,4 +9,12 @@ c= sorted(greedy_modularity_communities(G), key=len, reverse=True)
          # select `node_numbers' random nodes in this community to connect
       connect_nodes=list(rnd.sample(node_join_list, node_numbers))
        
-     
+      new_node_id= t+2*n-1
+         #an unique index for new node in each time step, you can assign whatever you like.
+         
+      G.add_node(new_node_id) 
+         
+      for i in range (node_numbers): #join community
+          G.add_edge(new_node_id, connect_nodes[i])
+             
+      
